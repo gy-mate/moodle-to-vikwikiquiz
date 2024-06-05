dc9e4416df3cf5c6672dc1132f176d2dd94aeaaf
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-06-05 02:32:38 +0200

    🔧 Fixed finding `src` by switching back to `find_namespace_packages()` & adding `package_dir=`

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup, find_packages  # type: ignore
+from setuptools import find_namespace_packages, setup  # type: ignore
 
 setup(
     name="moodle-to-vikwikiquiz",
     description="A CLI for converting a graded Moodle quiz HTML to a vik.wiki quiz wikitext.",
     author="Máté Gyöngyösi",
     url="https://github.com/gy-mate/homebrew-moodle-to-vikwikiquiz",
-    version="1.0.13",
-    packages=find_packages("src"),
+    version="1.0.14",
+    packages=find_namespace_packages("src"),
+    package_dir={"": "src"},
     entry_points={
         "console_scripts": [
             "moodle-to-vikwikiquiz=src.main:main",
