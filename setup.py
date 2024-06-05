e89ccc6275766a07f537410cf50119579042fe19
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-06-05 03:11:06 +0200

    ✏️ Converted `src` into a package

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     description="A CLI for converting a graded Moodle quiz HTML to a vik.wiki quiz wikitext.",
     author="Máté Gyöngyösi",
     url="https://github.com/gy-mate/homebrew-moodle-to-vikwikiquiz",
-    version="1.0.15",
+    version="1.0.16",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={
         "console_scripts": [
-            "moodle-to-vikwikiquiz=moodle_to_vikwikiquiz.main:main",
+            "moodle-to-vikwikiquiz=src.moodle_to_vikwikiquiz.main:main",
         ],
     },
     python_requires=">=3.9",
