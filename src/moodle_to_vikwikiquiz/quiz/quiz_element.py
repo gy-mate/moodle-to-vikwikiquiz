eee083f303cec1d70de0cf91b40b5ea5367209f0
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-08-18 14:31:38 +0200

    ➕ created QuizElement abstract class

---
+++ src/moodle_to_vikwikiquiz/quiz/quiz_element.py
@@ -0,0 +1,24 @@
+from abc import ABC
+
+# noinspection PyPackages
+from .illustration import Illustration  # type: ignore
+
+
+class QuizElement(ABC):
+    def __init__(
+        self,
+        text: str,
+        illustration: Illustration | None = None,
+    ) -> None:
+        assert isinstance(text, str)
+        self.text = text
+
+        if illustration:
+            assert isinstance(illustration, Illustration)
+        self.illustration = illustration
+
+    def __str__(self) -> str:
+        text = self.text
+        if self.illustration:
+            text += str(self.illustration)
+        return text
