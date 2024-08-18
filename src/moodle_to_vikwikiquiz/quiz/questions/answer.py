037609092f9ba1eae4990f99b07ce37fe99e27cc
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-08-18 14:32:03 +0200

    ➕ created Answer class

---
+++ src/moodle_to_vikwikiquiz/quiz/questions/answer.py
@@ -0,0 +1,13 @@
+# noinspection PyPackages
+from ..illustration import Illustration  # type: ignore
+
+# noinspection PyPackages
+from ..quiz_element import QuizElement  # type: ignore
+
+# noinspection PyPackages
+from ..state_of_illustrations import StateOfIllustrations  # type: ignore
+
+
+class Answer(QuizElement):
+    def __init__(self, text: str, illustration: Illustration | None = None) -> None:
+        super().__init__(text, illustration)
