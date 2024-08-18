f0c61bdfb591154cd8032cf3c08d6b63d6f121d6
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-08-18 14:34:57 +0200

    ➕ applied base class; implemented Answer & Illustration classes

--- src/moodle_to_vikwikiquiz/quiz/questions/question.py
+++ src/moodle_to_vikwikiquiz/quiz/questions/question.py
@@ -1,32 +1,43 @@
-# noinspection PyPackages
-from .grading_types import GradingType  # type: ignore
+from typing_extensions import override
 
 # noinspection PyPackages
-from .illustrations import StateOfIllustrations  # type: ignore
+from .answer import Answer  # type: ignore
+
+# noinspection PyPackages
+from ..grading_types import GradingType  # type: ignore
+
+# noinspection PyPackages
+from ..illustration import Illustration  # type: ignore
+
+# noinspection PyPackages
+from ..quiz_element import QuizElement  # type: ignore
+
+# noinspection PyPackages
+from ..state_of_illustrations import StateOfIllustrations  # type: ignore
 
 # noinspection PyPackages
 # future: report false positive to mypy developers
 from .question_types import QuestionType  # type: ignore
 
 
-class Question:
+class Question(QuizElement):
     def __init__(
         self,
         q_type: QuestionType,
         text: str,
-        illustration: StateOfIllustrations,
-        answers: list[str],
+        state_of_illustrations: StateOfIllustrations,
+        answers: list[Answer],
         correct_answers: set[int],
         grading: GradingType | None = None,
+        illustration: Illustration | None = None,
     ) -> None:
+        super().__init__(text, illustration)
+
         assert isinstance(q_type, QuestionType)
         self.q_type = q_type
 
-        assert isinstance(text, str)
-        self.text = text
-
-        assert isinstance(illustration, StateOfIllustrations)
-        self.illustration = illustration
+        assert isinstance(state_of_illustrations, StateOfIllustrations)
+        self.state_of_illustrations = state_of_illustrations
 
         assert isinstance(answers, list)
         assert answers
@@ -40,15 +51,15 @@ class Question:
             assert isinstance(grading, GradingType)
         self.grading = grading
 
+        if illustration:
+            assert isinstance(illustration, Illustration)
+        self.illustration = illustration
+
+    @override
     def __str__(self) -> str:
         text = f"== {self.text} =="
-        match self.illustration:
-            case StateOfIllustrations.YesAndAvailable:
-                raise NotImplementedError
-            case StateOfIllustrations.YesButNotAvailable:
-                text += "\n[[Fájl:.png|keret|keretnélküli|500x500px]]"
-            case StateOfIllustrations.Nil:
-                pass
+        if self.illustration:
+            text += str(self.illustration)
         ordered_correct_answers = list(self.correct_answers)
         ordered_correct_answers.sort()
         text += (
@@ -59,7 +70,7 @@ class Question:
             text += f"|pontozás={self.grading}"
         text += "}}"
         for answer in self.answers:
-            text += f"\n# {answer.replace("\n", " ")}"
+            text += f"\n# {str(answer).replace("\n", " ")}"
         return text
 
     def __hash__(self) -> int:
@@ -68,7 +79,7 @@ class Question:
                 (
                     self.q_type,
                     self.text,
-                    frozenset(sorted(self.answers)),
+                    frozenset(sorted([str(answer) for answer in self.answers])),
                     frozenset(self.correct_answers),
                     self.grading,
                 )
