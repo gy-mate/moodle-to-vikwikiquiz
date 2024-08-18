337b05a45a10ac301e263abda9a0500bfc84c604
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-08-18 14:32:26 +0200

    ➕ created Illustration class

---
+++ src/moodle_to_vikwikiquiz/quiz/illustration.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+# noinspection PyPackages
+from .state_of_illustrations import StateOfIllustrations  # type: ignore
+
+
+class Illustration:
+    def __init__(
+        self,
+        upload_filename: str,
+        size_in_pixels: int,
+        state_of_illustrations: StateOfIllustrations,
+        original_file_path: Path | None = None,
+    ) -> None:
+        assert isinstance(upload_filename, str)
+        self.upload_filename = upload_filename
+
+        assert isinstance(size_in_pixels, int)
+        self.size_in_pixels = size_in_pixels
+
+        assert isinstance(state_of_illustrations, StateOfIllustrations)
+        self.state_of_illustrations = state_of_illustrations
+
+        if original_file_path:
+            assert isinstance(original_file_path, Path)
+        self.original_file_path = original_file_path
+
+    def __str__(self) -> str:
+        if (
+            self.state_of_illustrations == StateOfIllustrations.YesAndAvailable
+            or self.state_of_illustrations == StateOfIllustrations.YesButNotAvailable
+        ):
+            return (
+                f"\n[[Fájl:{self.upload_filename}|keret|keretnélküli|{self.size_in_pixels}px]]"
+            )
+        else:
+            raise ValueError(
+                "No images were expected in the quiz but one image filename was requested!"
+            )
